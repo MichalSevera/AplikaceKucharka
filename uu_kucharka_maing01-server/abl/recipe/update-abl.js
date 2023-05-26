22c39294b76e6765dbcbc7cf96d63fb9a0d61bcc
Jiri Kodys <jirikodys@outlook.com>
2023-05-26 20:37:27 +0200

    navrh kodu na update

---
+++ uu_kucharka_maing01-server/abl/recipe/update-abl.js
@@ -0,0 +1,56 @@
+const Ajv = require("ajv").default;
+const dao = require("../../dao/recipe-dao");
+
+const userUtils = require("../UserUtils");
+
+let schema = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    userId: { type: "string" },
+    starred: { type: "boolean" }
+  },
+  required: ["id", "userId", "starred"],
+};
+
+function updateStars(recipe, userId, starred) {
+  let stars = recipe.starred ? [...recipe.starred] : [];
+  if (starred && !stars.includes(userId)){
+    stars.push(userId);
+  } else if (!starred && stars.includes(userId)) {
+    stars = stars.filter(s => s !== userId);
+  }
+  return stars;
+} 
+
+async function SetStarAbl(req, res) {
+  const ajv = new Ajv();
+  const valid = ajv.validate(schema, req.body);
+
+  if (!valid) {
+    res.status(400).json({
+      errorMessage: "Validation of input failed.",
+      params: req.body,
+      reason: ajv.errors,
+    });
+    return;
+  }
+
+  try {
+    const { id, userId, starred } = req.body;
+    let recipe = await dao.getRecipe(id);
+
+    if (!recipe) {
+      res.status(404).json({ errorMessage: `Recipe with id '${id}' doesn't exist.` });
+      return;
+    }
+
+    recipe = await dao.updateRecipe({id: recipe.id, starred: updateStars(recipe, userId, starred)});
+    res.json(recipe);
+
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+}
+
+module.exports = SetStarAbl;
