aed5f4928da38b940f12387014f173608627f630
Artem <bevzenko@seznam.cz>
2023-06-10 12:54:37 +0200

    pridani receptu

--- uu_kucharka_maing01-server/abl/UserUtils.js
+++ uu_kucharka_maing01-server/abl/UserUtils.js
@@ -4,12 +4,13 @@ const CREATOR = Symbol("Creator");
 const UserUtils = {
   ADMIN,
   CREATOR,
-  data: { // udržujte synchronizaci s development.json
-    [ADMIN] : ["15-8545-1", "88-2054-1", "256-5098-1"],
-    [CREATOR] : ["15-8545-1", "88-2054-1", "256-5098-1"]
+  data: {
+    // udržujte synchronizaci s development.json
+    [ADMIN]: ["15-8545-1", "88-2054-1", "256-5098-1", "7657-6217-1"],
+    [CREATOR]: ["15-8545-1", "88-2054-1", "256-5098-1", "7657-6217-1"],
   },
 
-  hasAuthority: function(userId, authority){
+  hasAuthority: function (userId, authority) {
     //console.log(this.data);
     if (this.data[authority] && Array.isArray(this.data[authority])) {
       return this.data[authority].includes(userId);
@@ -19,4 +20,3 @@ const UserUtils = {
 };
 
 module.exports = UserUtils;
-
